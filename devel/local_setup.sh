ef9fe09a56291473a9caf6ffe3c058309f269f9b
YS_ZH <piaojie150203@gmail.com>
2023-12-07 17:26:45 +0800

    after running catkin_make

--- devel/local_setup.sh
+++ devel/local_setup.sh
@@ -3,7 +3,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/shu233/woooork/autonomous_navigation/devel}
+: ${_CATKIN_SETUP_DIR:=/home/shu233/woooork/my_autonomous_navigation/移动机器人Course Project 1/devel}
 CATKIN_SETUP_UTIL_ARGS="--extend --local"
 . "$_CATKIN_SETUP_DIR/setup.sh"
 unset CATKIN_SETUP_UTIL_ARGS
--- devel/share/jackal_msgs/cmake/jackal_msgs-msg-paths.cmake
+++ devel/share/jackal_msgs/cmake/jackal_msgs-msg-paths.cmake
@@ -1,4 +1,4 @@
 # generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
 
-set(jackal_msgs_MSG_INCLUDE_DIRS "/home/shu233/woooork/autonomous_navigation/src/jackal/jackal_msgs/msg")
+set(jackal_msgs_MSG_INCLUDE_DIRS "/home/shu233/woooork/my_autonomous_navigation/移动机器人Course Project 1/src/jackal/jackal_msgs/msg")
 set(jackal_msgs_MSG_DEPENDENCIES std_msgs)
